9b097186f29c8dbe6c5e1f56ad164b70e48cf0f8
Victor Hugo <csivictorhugo@gmail.com>
2026-04-22 16:39:35 -0300

    feat: minimal api using fastify

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,36 @@
-import 'dotenv/config'
-import { env } from './config/env'
+import 'dotenv/config';
+import { env } from './config/env';
+import fastify from 'fastify';
 
-async function startServer() {
-  try {
-    console.log(`Server running on port ${env.PORT}`)
-  } catch (error) {
-    console.error('Failed to start application', error)
-    process.exit(1)
-  }
-}
+const server = fastify({logger: true})
 
-startServer()
+const teams = [
+    { id: 1, teamName: "Scuderia Ferrari HP", location: "Maranello, Italy"},
+    { id: 2, teamName: "Oracle Red Bull Racing", location: "Milton Keynes, United Kingdom"},
+    { id: 3, teamName: "Mercedes-AMG PETRONAS F1", location: "Brackley, Northamptonshire, UK"},
+]
+
+const drivers = [        
+  { id: 1, name: "Lewis Hamilton", surname: "HAM" },
+  { id: 1, name: "Charles Leclerc", surname: "LEC" },
+  { id: 2, name: "Max Verstappen", surname: "VER" },
+  { id: 2, name: "Isack Hadjar", surname: "HAD" },
+  { id: 3, name: "George Russell", surname: "RUS" },
+  { id: 3, name: "Andrea Kimi Antonelli", surname: "ANT" }
+]
+
+
+server.get("/teams", async(request, response) => {
+  response.type("application/json").code(200)
+  return {teams}
+})
+
+server.get("/drivers", async(request, response) => {
+  response.type("application/json").code(200)
+  return {drivers}
+})
+
+server.listen({ port: env.PORT}, () => {
+  console.log(`Server running on port ${env.PORT}`);
+  
+})
