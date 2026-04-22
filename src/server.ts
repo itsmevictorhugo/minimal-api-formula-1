bbb93d38cdb8904e72f235358ed33407fd68c548
Victor Hugo <csivictorhugo@gmail.com>
2026-04-22 17:32:01 -0300

    feat: improving query for drivers by id

--- src/server.ts
+++ src/server.ts
@@ -5,20 +5,19 @@ import fastify from 'fastify';
 const server = fastify({logger: true})
 
 const teams = [
-    { id: 1, teamName: "Scuderia Ferrari HP", location: "Maranello, Italy"},
-    { id: 2, teamName: "Oracle Red Bull Racing", location: "Milton Keynes, United Kingdom"},
-    { id: 3, teamName: "Mercedes-AMG PETRONAS F1", location: "Brackley, Northamptonshire, UK"},
+    { teamId: 1, teamName: "Scuderia Ferrari HP", location: "Maranello, Italy"},
+    { teamId: 2, teamName: "Oracle Red Bull Racing", location: "Milton Keynes, United Kingdom"},
+    { teamId: 3, teamName: "Mercedes-AMG PETRONAS F1", location: "Brackley, Northamptonshire, UK"},
 ]
 
 const drivers = [        
-  { id: 1, name: "Lewis Hamilton", surname: "HAM" },
-  { id: 1, name: "Charles Leclerc", surname: "LEC" },
-  { id: 2, name: "Max Verstappen", surname: "VER" },
-  { id: 2, name: "Isack Hadjar", surname: "HAD" },
-  { id: 3, name: "George Russell", surname: "RUS" },
-  { id: 3, name: "Andrea Kimi Antonelli", surname: "ANT" }
+  { driverId: 1, name: "Lewis Hamilton", surname: "HAM" },
+  { driverId: 2, name: "Charles Leclerc", surname: "LEC" },
+  { driverId: 3, name: "Max Verstappen", surname: "VER" },
+  { driverId: 4, name: "Isack Hadjar", surname: "HAD" },
+  { driverId: 5, name: "George Russell", surname: "RUS" },
+  { driverId: 6, name: "Andrea Kimi Antonelli", surname: "ANT" }
 ]
-
 
 server.get("/teams", async(request, response) => {
   response.type("application/json").code(200)
@@ -28,6 +27,24 @@ server.get("/teams", async(request, response) => {
 server.get("/drivers", async(request, response) => {
   response.type("application/json").code(200)
   return {drivers}
+})
+
+interface DriverParams{
+  id: string,
+  name: string,
+  surname: string
+}
+
+server.get<{Params: DriverParams}>("/drivers/:id", async(request, response) => {
+  const id = parseInt(request.params.id);
+  const driver = drivers.find(d => d.driverId === id);
+  if(!driver) {
+    response.type("application/json").code(404)
+    return { message: "Driver Not Found"}
+  } else {
+    response.type("application/json").code(200)
+    return { driver }
+  }
 })
 
 server.listen({ port: env.PORT}, () => {
